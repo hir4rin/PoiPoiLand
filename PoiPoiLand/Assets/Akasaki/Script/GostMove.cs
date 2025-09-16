76c6c06e3e1efa7bb46bf54f742db670a4e10795
harukaakasaki <harukajobstar@gmail.com>
2025-09-16 18:51:15 +0900

    up

--- PoiPoiLand/Assets/Akasaki/Script/GostMove.cs
+++ PoiPoiLand/Assets/Akasaki/Script/GostMove.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class GostMove : MonoBehaviour
 {
+    Vector3 startPos; // 初期位置
+    Vector3 pos; //更新された位置
+
+    Transform playerTr; //プレイヤーのトランスフォーム
+    [SerializeField] float speed = 2; // 敵の動くスピード
+    [SerializeField] float followRange = 5.0f; // 追従距離
+    [SerializeField] float floatHeight = 0.5f;
+    [SerializeField] float floatSpeed = 2.0f;
+    [SerializeField] float wanderRange = 1.0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        playerTr = GameObject.FindGameObjectWithTag("Player").transform;
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, playerTr.position);
+
+        // プレイヤーが一定範囲内に入った場合
+        if(distance < followRange)
+        {
+            //プレイヤーに向かって進む
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                playerTr.position,
+                //new Vector3(playerTr.position.x, playerTr.position.y),
+                speed * Time.deltaTime);
+
+            pos = transform.position;
+            Debug.Log("入ってきたやん");
+        }
+
+        else
+        {
+            //幽霊っぽい挙動
+            //float yOffset = Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+            //float xOffset = Mathf.Sin(Time.time * (floatSpeed * 0.5f)) * wanderRange;
+
+            //transform.position = new Vector3(
+            //    pos.x + xOffset,
+            //    pos.y + yOffset,
+            //    pos.z);
+            Debug.Log("誰も来ないやん");
+        }
+
     }
 }
