8b85963ee1068c5ae24ae21c01b365d8831df1b1
KaminagaAyumu <kaminagajobsearch@gmail.com>
2025-09-16 18:52:47 +0900

    up

--- PoiPoiLand/Assets/Kaminaga/Script/Ground_Test_1.cs
+++ PoiPoiLand/Assets/Kaminaga/Script/Ground_Test_1.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Ground_Test_1 : MonoBehaviour
 {
+    Rigidbody rb;
     Vector3 pos;
+    Vector3 up;
+    Vector3 x;
+    Vector3 y;
+    Vector3 z;
     int testCount;
     int hitCount;
+    float test;
     private void OnCollisionEnter(Collision collision)
     {
         hitCount++;
@@ -20,22 +26,41 @@ public class Ground_Test_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //pos = new Vector3(0.0f,transform.position.y,0.0f);
+        rb = GetComponent<Rigidbody>();
+        up = new Vector3(0.0f, -0.05f, 0.10f);
+        x = new Vector3(0.30f, 0.0f, 0.0f);
+        y = new Vector3(0.0f,0.30f, 0.0f);
+        z = new Vector3(0.0f, 0.0f, 0.30f);
         testCount = 0;
         hitCount = 0;
+        test = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        testCount++;
-        if (testCount >= 120)
+        if (Input.GetKey(KeyCode.A))
         {
-            pos.x = 2.8f;
-            pos.y = 4.0f;
-            this.transform.position = pos;
-            testCount = 0;
+            rb.AddForce(x, ForceMode.Impulse);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(y, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(z, ForceMode.Impulse);
+        }
+        //testCount++;
+        //if (testCount >= 120)
+        //{
+        //    this.transform.position += pos;
+        //    testCount = 0;
+        //}
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    pos.y -= 1.0f;
+        //}
         //pos.z += Input.GetAxis("Vertical");
         //pos.x += Input.GetAxis("Horizontal");
         //transform.position = pos;
