1fc75bed58146c29677bee40355540a963a270da
ko1307 <208754810+13ko13@users.noreply.github.com>
2025-09-30 22:03:31 +0900

    up

--- PoiPoiLand/Assets/HiraoTaiki/Script/Player/Player.cs
+++ PoiPoiLand/Assets/HiraoTaiki/Script/Player/Player.cs
@@ -260,33 +260,33 @@ public class Player : MonoBehaviour
     }
     public void Death()
     {
-        ////ここでチェックポイントによって座標を変える
-        // Debug.Log($"{PlayerPrefs.GetInt("PointNum")}");
-        //switch (PlayerPrefs.GetInt("PointNum"))
-        //{
-        //    case 0:
-        //        this.transform.position = _checkPoint.StartPos;
-        //        break;
-        //    case 1:
-        //        this.transform.position = _checkPoint.warpToFirstStage;
-        //        break;
-        //    case 2:
-        //        this.transform.position = _checkPoint.warpToMapFirst;
-        //        break;
-        //    case 3:
-        //        this.transform.position = _checkPoint.warpToSecondStage;
-        //        break;
-        //    case 4:
-        //        this.transform.position = _checkPoint.warpToMapSecond;
-        //        break;
-        //    case 5:
-        //        this.transform.position = _checkPoint.warpToThirdStage;
-        //        break;
-        //    case 6:
-        //        this.transform.position = _checkPoint.warpToMapThird;
-        //        break;
-        //    default:
-        //        break;
-        //}
+        //ここでチェックポイントによって座標を変える
+        Debug.Log($"{PlayerPrefs.GetInt("PointNum")}");
+        switch (PlayerPrefs.GetInt("PointNum"))
+        {
+            case 0:
+                this.transform.position = _checkPoint.StartPos;
+                break;
+            case 1:
+                this.transform.position = _checkPoint.warpToFirstStage;
+                break;
+            case 2:
+                this.transform.position = _checkPoint.warpToMapFirst;
+                break;
+            case 3:
+                this.transform.position = _checkPoint.warpToSecondStage;
+                break;
+            case 4:
+                this.transform.position = _checkPoint.warpToMapSecond;
+                break;
+            case 5:
+                this.transform.position = _checkPoint.warpToThirdStage;
+                break;
+            case 6:
+                this.transform.position = _checkPoint.warpToMapThird;
+                break;
+            default:
+                break;
+        }
     }
 }
