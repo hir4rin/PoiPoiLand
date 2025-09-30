e6816de55bb8fba3f2005df924f8fcc31154ce36
hir4rin <fko2547051@stu.o-hara.ac.jp>
2025-09-30 18:54:53 +0900

    up

---
+++ PoiPoiLand/Assets/HiraoTaiki/Script/StageSecond/RabbitJenerator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitJenerator : MonoBehaviour
+{
+    [SerializeField] GameObject _rabbit;
+     
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ラビットを固定位置に生成(関数)
+        //ラビットが亀に触れると倒れる
+            
+    }
+}
