41227d2ceb55d17ffabb7a134cca775d5cb1e3aa
hir4rin <fko2547051@stu.o-hara.ac.jp>
2025-10-07 18:57:59 +0900

    up

---
+++ PoiPoiLand/Assets/HiraoTaiki/Movie/movieManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+
+public class movieManager : MonoBehaviour
+{
+   public  GameObject _chest;
+    public GameObject _mimic;
+
+   public  bool isChange = false;
+   public  Animator animator;         // Animatorコンポーネントを保持
+    bool animEndFlag = false; // アニメが終わったかどうか
+    string endStateName = "Chest_Close 0"; // Animator上のステート名
+
+    //2回目
+   public  Animator animator2;         // Animatorコンポーネントを保持
+    bool animEndFlag2 = false; // アニメが終わったかどうか
+    string endStateName2 = "anim_Mimic_BattleStand"; // Animator上のステート名
+
+    [SerializeField] GameObject _line;
+    [SerializeField] CinemachineVirtualCamera lastCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mimic.SetActive(false);
+        _line.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        // 現在のステート情報を取得
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        AnimatorStateInfo stateInfo2 = animator2.GetCurrentAnimatorStateInfo(0);
+        // 第0レイヤー（通常のアニメ用レイヤー）
+      
+
+        // 現在のステートが「Attack」で、再生率が1.0以上＝アニメが終わった
+        if (stateInfo.IsName(endStateName) && stateInfo.normalizedTime >= 2.0f)
+        {
+            if (!animEndFlag)
+            {
+                isChange = true;
+                animEndFlag = true;
+                Debug.Log("アニメーションが終了しました！");
+                // ここにフラグを立てたあとに実行したい処理を追加する
+            }
+        }
+        else
+        {
+            // Attack以外のステートなら、また次に備えてリセット
+            animEndFlag = false;
+        }
+        //2回目(カメラ用)
+        if (stateInfo2.IsName(endStateName2) && stateInfo2.normalizedTime >= 0.5f)
+        {
+            if (!animEndFlag2)
+            {
+                Debug.Log("aaaaaaaaaaaaaaaaaa");
+                animEndFlag2 = true;
+                lastCam.Priority = 30;
+            }
+        }
+        else
+        {
+            
+            animEndFlag2 = false;
+        }
+
+        if (isChange)
+        {
+            _chest.SetActive(false);
+            _mimic.SetActive(true);
+            _line.SetActive(true) ;
+            
+        }
+        Debug.Log("現在のアニメステート: " + stateInfo2.IsName("anim_Mimic_BattleStand"));
+    }
+}
---
+++ PoiPoiLand/Assets/HiraoTaiki/Movie/playerMovie.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovie : MonoBehaviour
+{
+
+    //移動スピード
+    float speed = 0.08f;
+    Animator animator;
+    [SerializeField] movieManager _movieManager;
+
+    bool prevChange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed > 0)
+        {
+            animator.SetBool("isWalk", true);
+        }
+        
+        transform.position += new Vector3(0, 0, 1) * speed * speed;//前進
+        if(transform.position.z > -3.7)
+        {
+            animator.SetBool("isWalk", false);
+            _movieManager.animator.SetTrigger("isOpen");
+            speed = 0;
+        }
+        if (_movieManager.isChange && !prevChange)
+        {
+            animator.SetTrigger("TriggerJump");
+        }
+
+        prevChange = _movieManager.isChange;
+    }
+
+}
