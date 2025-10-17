7383f97da88ead57a952b1f1a076ec673e41e4fd
hir4rin <fko2547051@stu.o-hara.ac.jp>
2025-10-17 19:00:45 +0900

    up

--- PoiPoiLand/Assets/Kaminaga/Script/CameraFollowCollection.cs
+++ PoiPoiLand/Assets/Kaminaga/Script/CameraFollowCollection.cs
@@ -19,7 +19,6 @@ public class CameraFollowCollection : MonoBehaviour
     void Start()
     {
         lastPlayerPos = currentPlayerPos.position;
-        threshold = new Vector3(0.0f, 1.0f, 0.0f);
     }
 
     // Update is called once per frame
--- PoiPoiLand/Assets/Kaminaga/Script/DolphinHigh.cs
+++ PoiPoiLand/Assets/Kaminaga/Script/DolphinHigh.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class DolphinHigh : MonoBehaviour
 {
+
+    private Animator m_animator;
+    private float m_animTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_animator = GetComponent<Animator>();
+        m_animTime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(m_animTime);
+        m_animTime += Time.deltaTime;
+        if (m_animTime >= 3.0f)
+        {
+            if (!m_animator.GetBool("isMove"))
+            {
+                m_animator.SetBool("isMove", true);
+            }
+            else
+            {
+                m_animator.SetBool("isMove", false);
+            }
+
+            m_animTime = 0.0f;
+        }
     }
 }
